8498cbe44d009ba76234dcba69f52ba2f55fd4c0
Michael Shin <michaelshin@Michaels-MBP-3.lan>
2023-01-05 22:15:14 -0800

    added animations using animate.css

--- script.js
+++ script.js
@@ -73,6 +73,7 @@ const insertLetter = (pressedKey) => {
 
   let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
   let box = row.children[nextLetter];
+  animateCSS(box, "pulse");
   box.textContent = pressedKey;
   box.classList.add("filled-box");
   currentGuess.push(pressedKey);
@@ -128,6 +129,9 @@ const checkGuess = () => {
     }
     let delay = 250 * i;
     setTimeout(() => {
+      //flip box
+      animateCSS(box, "flipInX");
+      //shade boxs
       box.style.backgroundColor = letterColor;
       shadeKeyBoard(letter, letterColor);
     }, delay);
@@ -210,3 +214,23 @@ document.getElementById("keyboard-cont").addEventListener("click", (e) => {
 });
 
 // how to add animation
+
+const animateCSS = (element, animation, prefix = "animate__") =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    // const node = document.querySelector(element);
+    const node = element;
+    node.style.setProperty("--animate-duration", "0.3s");
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve("Animation ended");
+    }
+
+    node.addEventListener("animationend", handleAnimationEnd, { once: true });
+  });
